e09206f870d2ea9f4b92dfedb5117b16ff695f7c
Mohammad Shoubash <95358258+moshoubash@users.noreply.github.com>
2024-02-12 19:06:58 +0300

    Add files via upload

---
+++ DotnetNewProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace MyApplication
+{
+    public class Program {
+        static void Main(string[] args) {
+            C obj = new C();
+            obj.fn();
+        }
+    }
+
+    public class C {
+        public void fn() {
+            int[] arr = {1, 2, 3, 4};
+            foreach (var item in arr)
+            {
+                Console.Write($"{item}\t");
+            }
+        }
+    }
+}
